c409c3e11a17700f0489803dce132677c2544cf8
Alexander <alexp0111@mail.ru>
2021-11-27 21:18:38 +0300

    Add all cards to collection and 8\11 images

--- app/src/main/java/com/example/lbar/fragments/mainMenuFragments/CollectionFragment.java
+++ app/src/main/java/com/example/lbar/fragments/mainMenuFragments/CollectionFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +20,8 @@ import androidx.fragment.app.Fragment;
 
 import com.example.lbar.MainActivity;
 import com.example.lbar.R;
+import com.google.android.material.card.MaterialCardView;
+
 
 import static com.example.lbar.MainActivity.SWIPE_THRESHOLD;
 import static com.example.lbar.MainActivity.SWIPE_VELOCITY_THRESHOLD;
@@ -25,6 +29,21 @@ import static com.example.lbar.MainActivity.SWIPE_VELOCITY_THRESHOLD;
 public class CollectionFragment extends Fragment implements GestureDetector.OnGestureListener {
 
     private DrawerLayout drawer;
+    private ScrollView sv;
+
+    private static MaterialCardView cubeType0;
+    private static MaterialCardView cubeType1;
+    private static MaterialCardView cubeType2;
+    private static MaterialCardView cubeType3;
+    private static MaterialCardView cubeType4;
+    private static MaterialCardView cubeType5;
+    private static MaterialCardView cubePyraminx;
+    private static MaterialCardView cubeSqube;
+    private static MaterialCardView cubeClock;
+    private static MaterialCardView cubeMegaminx;
+    private static MaterialCardView cubeSquare1;
+
+
 
     private GestureDetector gestureDetector;
 
@@ -37,19 +56,54 @@ public class CollectionFragment extends Fragment implements GestureDetector.OnGe
 
         Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar_in_collection);
         setToolbarSettings(toolbar, activity, main_activity);
+        
+        initItems(view);
+
+        realiseClickListenerOnCards();
 
         gestureDetector = new GestureDetector(getContext(), this);
 
-        SwipeMenuOpenerControl(view);
+        SwipeMenuOpenerControl(sv);
 
         return view;
+    }
+
+    private void initItems(View v) {
+        sv = v.findViewById(R.id.scrollView_in_collection);
+
+        cubeType0 = v.findViewById(R.id.collection_card_type0);
+        cubeType1 = v.findViewById(R.id.collection_card_type1);
+        cubeType2 = v.findViewById(R.id.collection_card_type2);
+        cubeType3 = v.findViewById(R.id.collection_card_type3);
+        cubeType4 = v.findViewById(R.id.collection_card_type4);
+        cubeType5 = v.findViewById(R.id.collection_card_type5);
+
+        cubePyraminx = v.findViewById(R.id.collection_card_pyraminx);
+        cubeSqube = v.findViewById(R.id.collection_card_sqube);
+        cubeClock = v.findViewById(R.id.collection_card_clock);
+        cubeMegaminx = v.findViewById(R.id.collection_card_megaminx);
+        cubeSquare1 = v.findViewById(R.id.collection_card_square1);
+    }
+
+    private void realiseClickListenerOnCards() {
+        cubeType0.setOnClickListener(view -> Toast.makeText(getContext(), "2 x 2", Toast.LENGTH_SHORT).show());
+        cubeType1.setOnClickListener(view -> Toast.makeText(getContext(), "3 x 3", Toast.LENGTH_SHORT).show());
+        cubeType2.setOnClickListener(view -> Toast.makeText(getContext(), "4 x 4", Toast.LENGTH_SHORT).show());
+        cubeType3.setOnClickListener(view -> Toast.makeText(getContext(), "5 x 5", Toast.LENGTH_SHORT).show());
+        cubeType4.setOnClickListener(view -> Toast.makeText(getContext(), "6 x 6", Toast.LENGTH_SHORT).show());
+        cubeType5.setOnClickListener(view -> Toast.makeText(getContext(), "7 x 7", Toast.LENGTH_SHORT).show());
+        cubeClock.setOnClickListener(view -> Toast.makeText(getContext(), "Clocks", Toast.LENGTH_SHORT).show());
+        cubePyraminx.setOnClickListener(view -> Toast.makeText(getContext(), "Pyraminx", Toast.LENGTH_SHORT).show());
+        cubeMegaminx.setOnClickListener(view -> Toast.makeText(getContext(), "Megaminx", Toast.LENGTH_SHORT).show());
+        cubeSqube.setOnClickListener(view -> Toast.makeText(getContext(), "Sqube", Toast.LENGTH_SHORT).show());
+        cubeSquare1.setOnClickListener(view -> Toast.makeText(getContext(), "Square-1", Toast.LENGTH_SHORT).show());
     }
 
     private void SwipeMenuOpenerControl(View v) {
         v.setOnTouchListener((view1, motionEvent) -> {
             view1.performClick();
             gestureDetector.onTouchEvent(motionEvent);
-            return true;
+            return false;
         });
     }
 
@@ -114,5 +168,4 @@ public class CollectionFragment extends Fragment implements GestureDetector.OnGe
     public void onLongPress(MotionEvent motionEvent) {
 
     }
-
 }
