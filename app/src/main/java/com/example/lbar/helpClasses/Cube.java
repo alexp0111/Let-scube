ab9cc98f4a3e9aee7a80cda0a14527b34d912638
Alexander <alexp0111@mail.ru>
2022-03-11 23:06:40 +0300

    Collection bottom sheet -> hardcoded blank to refactor -> update Cube class -> getting right format data added

--- app/src/main/java/com/example/lbar/helpClasses/Cube.java
+++ app/src/main/java/com/example/lbar/helpClasses/Cube.java
@@ -7,10 +7,10 @@ public class Cube {
     private int cube_type;
     private String cube_name;
 
-    private ArrayList<String> puzzle_build_pb_statistics = new ArrayList<>(); //pb, pb3, pb5, pb12, pb100;
-    private ArrayList<String> puzzle_build_avg_statistics = new ArrayList<>();
+    private ArrayList<Long> puzzle_build_pb_statistics = new ArrayList<>(); //pb, pb3, pb5, pb12, pb100;
+    private ArrayList<Long> puzzle_build_avg_statistics = new ArrayList<>();
 
-    public Cube(int cube_type, String cube_name, ArrayList<String> puzzle_build_pb_statistics, ArrayList<String> puzzle_build_avg_statistics) {
+    public Cube(int cube_type, String cube_name, ArrayList<Long> puzzle_build_pb_statistics, ArrayList<Long> puzzle_build_avg_statistics) {
         this.cube_type = cube_type;
         this.cube_name = cube_name;
 
@@ -22,16 +22,49 @@ public class Cube {
         this.cube_type = cube_type;
     }
 
-    public String convertFromMStoString(long ms){
+    public String getBestInfo() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < puzzle_build_pb_statistics.size() - 1; i++) {
+            result.append(convertFromMStoString(puzzle_build_pb_statistics.get(i))).append("\n");
+        }
+        result.append(convertFromMStoString(puzzle_build_pb_statistics.get(puzzle_build_pb_statistics.size() - 1)));
+        return result.toString();
+    }
+
+    public String getAvgInfo() {
+        StringBuilder result = new StringBuilder();
+        result.append(convertFromMStoString(getThisAvg(1))).append("\n");
+        result.append(convertFromMStoString(getThisAvg(3))).append("\n");
+        result.append(convertFromMStoString(getThisAvg(5))).append("\n");
+        result.append(convertFromMStoString(getThisAvg(12))).append("\n");
+        result.append(convertFromMStoString(getThisAvg(100)));
+        return result.toString();
+    }
+
+    private Long getThisAvg(int number) {
+        Long result = 0L;
+        for (int i = puzzle_build_avg_statistics.size() - 1; i < puzzle_build_avg_statistics.size() - number; i++) {
+            result += puzzle_build_avg_statistics.get(i);
+        }
+        return result / number;
+    }
+
+    private String convertFromMStoString(long ms) {
         String result = "";
-        result += strCorrection(ms / 3600000, 2) + ":"; ms %= 3600000;
-        result += strCorrection(ms / 60000, 2) + ":"; ms %= 60000;
+        if (ms / 3600000 != 0) {
+            result += strCorrection(ms / 3600000, 2) + ":";
+        }
+        ms %= 3600000;
+        if (ms / 60000 != 0) {
+            result += strCorrection(ms / 60000, 2) + ":";
+        }
+        ms %= 60000;
         result += strCorrection(ms / 1000, 2) + ":" + strCorrection(ms %= 1000, 3);
 
         return result;
     }
 
-    private String strCorrection(long numToCorrect, int digits){
+    private String strCorrection(long numToCorrect, int digits) {
         StringBuilder num = new StringBuilder(Long.toString(numToCorrect));
         while (num.length() < digits) num.insert(0, "0");
         return num.toString();
@@ -39,10 +72,6 @@ public class Cube {
 
     public int getCube_type() {
         return cube_type;
-    }
-
-    public void setCube_type(int cube_type) {
-        this.cube_type = cube_type;
     }
 
     public String getCube_name() {
@@ -53,19 +82,19 @@ public class Cube {
         this.cube_name = cube_name;
     }
 
-    public ArrayList<String> getPuzzle_build_pb_statistics() {
+    public ArrayList<Long> getPuzzle_build_pb_statistics() {
         return puzzle_build_pb_statistics;
     }
 
-    public void setPuzzle_build_pb_statistics(ArrayList<String> puzzle_build_pb_statistics) {
+    public void setPuzzle_build_pb_statistics(ArrayList<Long> puzzle_build_pb_statistics) {
         this.puzzle_build_pb_statistics = puzzle_build_pb_statistics;
     }
 
-    public ArrayList<String> getPuzzle_build_avg_statistics() {
+    public ArrayList<Long> getPuzzle_build_avg_statistics() {
         return puzzle_build_avg_statistics;
     }
 
-    public void setPuzzle_build_avg_statistics(ArrayList<String> puzzle_build_avg_statistics) {
+    public void setPuzzle_build_avg_statistics(ArrayList<Long> puzzle_build_avg_statistics) {
         this.puzzle_build_avg_statistics = puzzle_build_avg_statistics;
     }
 }
