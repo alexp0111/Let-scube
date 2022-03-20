8080afb41662c549109c7d92e80152da079ff0ba
Alexander <alexp0111@mail.ru>
2022-03-20 17:45:49 +0300

    Solo cubing room [second edit] realising start\stop mechanic

--- app/src/main/java/com/example/lbar/fragments/mainMenuFragments/roomsFragments/RoomsSoloFragment.java
+++ app/src/main/java/com/example/lbar/fragments/mainMenuFragments/roomsFragments/RoomsSoloFragment.java
@@ -1,13 +1,17 @@
 package com.example.lbar.fragments.mainMenuFragments.roomsFragments;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Chronometer;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,7 +28,10 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class RoomsSoloFragment extends Fragment {
 
+    private boolean isRunning = false;
+
     private TextView chronometer;
+    private LinearLayout layout;
 
     private MaterialButton plusTwo;
     private MaterialButton buttonStart;
@@ -38,6 +45,7 @@ public class RoomsSoloFragment extends Fragment {
     private long timeSwapBuffer = 0L;
     private long updateTime = 0L;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_solo_rooms, container, false);
@@ -47,6 +55,7 @@ public class RoomsSoloFragment extends Fragment {
         plusTwo = view.findViewById(R.id.button_plus_two);
         buttonStart = view.findViewById(R.id.rooms_solo_start_btn);
         buttonStop = view.findViewById(R.id.rooms_solo_stop_btn);
+        layout = view.findViewById(R.id.layout_solo_main);
 
         customHandler = new Handler(Looper.getMainLooper());
 
@@ -71,23 +80,61 @@ public class RoomsSoloFragment extends Fragment {
             }
         });
 
-
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startTime = SystemClock.uptimeMillis();
-                customHandler.postDelayed(updateTimerThread, 0);
+        layout.setOnTouchListener((view12, motionEvent) -> {
+            switch (motionEvent.getAction() ) {
+                case MotionEvent.ACTION_DOWN:
+                    if (isRunning == false){
+                        chronometer.setText("00:000");
+                        layout.setBackgroundColor(Color.GREEN); // pressed state
+                    } else {
+                        Snackbar.make(getView(), "Time is: " + chronometer.getText(), BaseTransientBottomBar.LENGTH_SHORT).show();
+                        startTime = 0L;
+                        timeInMS = 0L;
+                        timeSwapBuffer = 0L;
+                        updateTime = 0L;
+                        customHandler.removeCallbacks(updateTimerThread);
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if (isRunning == false){
+                        startTime = SystemClock.uptimeMillis();
+                        customHandler.postDelayed(updateTimerThread, 0);
+                        layout.setBackgroundResource(R.color.colorPrimary);
+                        isRunning = true;
+                    } else {
+                        isRunning = false;
+                    }
+                    break;
             }
+            return true;
         });
 
-        buttonStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(getView(), "Time is: " + chronometer.getText(), BaseTransientBottomBar.LENGTH_SHORT).show();
-                timeSwapBuffer+=timeInMS;
-                customHandler.removeCallbacks(updateTimerThread);
-            }
-        });
+        //layout.setOnLongClickListener(view14 -> {
+        //    startTime = SystemClock.uptimeMillis();
+        //    customHandler.postDelayed(updateTimerThread, 0);
+        //    return true;
+        //});
+//
+        //layout.setOnClickListener(view15 -> {
+        //    Snackbar.make(getView(), "Time is: " + chronometer.getText(), BaseTransientBottomBar.LENGTH_SHORT).show();
+        //    startTime = 0L;
+        //    timeInMS = 0L;
+        //    timeSwapBuffer = 0L;
+        //    updateTime = 0L;
+        //    customHandler.removeCallbacks(updateTimerThread);
+        //});
+
+
+        //buttonStart.setOnClickListener(view13 -> {
+        //    startTime = SystemClock.uptimeMillis();
+        //    customHandler.postDelayed(updateTimerThread, 0);
+        //});
+//
+        //buttonStop.setOnClickListener(view12 -> {
+        //    Snackbar.make(getView(), "Time is: " + chronometer.getText(), BaseTransientBottomBar.LENGTH_SHORT).show();
+        //    timeSwapBuffer+=timeInMS;
+        //    customHandler.removeCallbacks(updateTimerThread);
+        //});
 
         return view;
     }
