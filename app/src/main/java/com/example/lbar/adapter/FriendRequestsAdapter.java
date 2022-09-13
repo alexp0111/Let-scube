ac85ee80f1b99c1e012c36413cdcfae4007dbf28
Alexander <alexp0111@mail.ru>
2022-09-13 15:12:26 +0300

    Friends interactions [5.1-th step] - interactions [done basic mechanic] [v2.0]

--- app/src/main/java/com/example/lbar/adapter/FriendRequestsAdapter.java
+++ app/src/main/java/com/example/lbar/adapter/FriendRequestsAdapter.java
@@ -13,10 +13,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.lbar.R;
+import com.example.lbar.helpClasses.FriendRequest;
 import com.example.lbar.helpClasses.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FriendRequestsAdapter extends RecyclerView.Adapter<FriendRequestsAdapter.ViewHolder> {
@@ -24,15 +29,15 @@ public class FriendRequestsAdapter extends RecyclerView.Adapter<FriendRequestsAd
     private static String TAG = "FRIEND_REQUESTS_ADAPTER";
 
     private List<User> mUsers;
-    private List<String> requestsIDs;
+    private List<FriendRequest> requestsList;
     private String userID;
     private Context mContext;
 
-    public FriendRequestsAdapter(List<User> mUsers, List<String> requestsIDs, String userID, Context mContext) {
+    public FriendRequestsAdapter(List<User> mUsers, List<FriendRequest> requestsList, String userID, Context mContext) {
         this.mUsers = mUsers;
         this.mContext = mContext;
         this.userID = userID;
-        this.requestsIDs = requestsIDs;
+        this.requestsList = requestsList;
     }
 
     @NonNull
@@ -49,27 +54,73 @@ public class FriendRequestsAdapter extends RecyclerView.Adapter<FriendRequestsAd
         holder.username.setText(requester.getUs_name());
         Glide.with(mContext).load(requester.getImage()).into(holder.profile_image);
         holder.add_button.setOnClickListener(view -> {
-            //DatabaseReference ref1 = FirebaseDatabase.getInstance("https://lbar-messenger-default-rtdb.firebaseio.com/")
-            //        .getReference("Users")
-            //        .child(requester.getUs_id())
-            //        .child("us_friends");
-//
-            //DatabaseReference ref2 = FirebaseDatabase.getInstance("https://lbar-messenger-default-rtdb.firebaseio.com/")
-            //        .getReference("Users")
-            //        .child(userID)
-            //        .child("us_friends");
-//
-            //friendIDs.remove(friend.getUs_id());
-            //friendIDs.set(0, String.valueOf(friendIDs.size() - 1));
-//
-            //// Deleting from our user list
-            //ref2.setValue(friendIDs).addOnCompleteListener(task -> {
-            //    Log.d(TAG, position + " " + friendIDs.size() + " " + mUsers.size());
-            //    removeRecyclerElement(friend);
-            //});
-//
-            // TODO: adding friend + removing request
+            ArrayList<String> usFriendsList = new ArrayList<>();
+            ArrayList<String> reqFriendsList = new ArrayList<>();
+
+
+            DatabaseReference refReqFriends = FirebaseDatabase.getInstance("https://lbar-messenger-default-rtdb.firebaseio.com/")
+                    .getReference("Users")
+                    .child(requester.getUs_id())
+                    .child("us_friends");
+
+            DatabaseReference refUsFriends = FirebaseDatabase.getInstance("https://lbar-messenger-default-rtdb.firebaseio.com/")
+                    .getReference("Users")
+                    .child(userID)
+                    .child("us_friends");
+
+            DatabaseReference requestRef = FirebaseDatabase.getInstance("https://lbar-messenger-default-rtdb.firebaseio.com/")
+                    .getReference("FriendRequests");
+
+            // getting this user friends
+            refUsFriends.get().addOnCompleteListener(task -> {
+                for (DataSnapshot snapshot : task.getResult().getChildren()) {
+                    usFriendsList.add(snapshot.getValue().toString());
+                }
+
+                Log.d(TAG, "Us_friends_list_get; Size: " + usFriendsList.size());
+            }).addOnCompleteListener(task2 -> refReqFriends.get().addOnCompleteListener(task3 -> {
+                // getting requester friends
+                for (DataSnapshot snapshot : task3.getResult().getChildren()) {
+                    reqFriendsList.add(snapshot.getValue().toString());
+                }
+
+                Log.d(TAG, "Requester_friends_list_get; Size: " + usFriendsList.size());
+            }).addOnCompleteListener(task4 -> {
+                usFriendsList.add(requester.getUs_id());
+                reqFriendsList.add(userID);
+
+                usFriendsList.set(0, String.valueOf(usFriendsList.size()-1));
+                reqFriendsList.set(0, String.valueOf(reqFriendsList.size()-1));
+
+                Log.d(TAG, "Friends_interactions_in_lists: DONE");
+                Log.d(TAG, "US_friends_list_get; Size: " + usFriendsList.size());
+                Log.d(TAG, "Requester_friends_list_get; Size: " + reqFriendsList.size());
+                refUsFriends.setValue(usFriendsList).addOnCompleteListener(task5 -> refReqFriends.setValue(reqFriendsList).addOnCompleteListener(task6 -> {
+                    requestRef.child(getRequest(requester)).removeValue().addOnCompleteListener(task7 -> {
+                        removeRecyclerElement(requester);
+                        Log.d(TAG, "item removed");
+                    });
+                }));
+            }));
         });
+    }
+
+    private String getRequest(User requester) {
+        for (int i = 0; i < requestsList.size(); i++) {
+            if (requestsList.get(i).getToID().equals(userID) && requestsList.get(i).getFromID().equals(requester.getUs_id()))
+                return requestsList.get(i).getRequestID();
+        }
+        return "";
+    }
+
+    private void removeRecyclerElement(User requester) {
+        String idToDelete = requester.getUs_id();
+        for (int i = 0; i < mUsers.size(); i++) {
+            if (mUsers.get(i).getUs_id().equals(idToDelete)) {
+                mUsers.remove(i);
+                notifyItemRemoved(i);
+            }
+        }
     }
 
     @Override
--- app/src/main/java/com/example/lbar/fragments/mainMenuFragments/peopleFragments/friendsFragments/FriendsInteractionsFragment.java
+++ app/src/main/java/com/example/lbar/fragments/mainMenuFragments/peopleFragments/friendsFragments/FriendsInteractionsFragment.java
@@ -14,7 +14,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.lbar.R;
 import com.example.lbar.adapter.FriendRequestsAdapter;
-import com.example.lbar.adapter.FriendsAdapter;
 import com.example.lbar.helpClasses.FriendRequest;
 import com.example.lbar.helpClasses.User;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -39,16 +38,16 @@ public class FriendsInteractionsFragment extends Fragment {
     private FirebaseAuth mAuth;
     private String usId;
 
-    private ArrayList<String> requestsIDList;
-    private ArrayList<User> requestsList;
+    private ArrayList<FriendRequest> requestsList;
+    private ArrayList<User> requestersList;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_friends_interactions, container, false);
 
-        requestsIDList = new ArrayList<>();
         requestsList = new ArrayList<>();
+        requestersList = new ArrayList<>();
 
         requestsRef = FirebaseDatabase.getInstance().getReference("FriendRequests");
         userRef = FirebaseDatabase.getInstance().getReference("Users");
@@ -72,34 +71,34 @@ public class FriendsInteractionsFragment extends Fragment {
 
     private void getFriendRequestsList() {
         requestsRef.get().addOnCompleteListener(task -> {
-            requestsIDList.clear();
+            requestsList.clear();
 
             for (DataSnapshot snapshot : task.getResult().getChildren()) {
                 FriendRequest request = snapshot.getValue(FriendRequest.class);
 
                 if (request != null && request.getToID().equals(usId))
-                    requestsIDList.add(request.getFromID());
+                    requestsList.add(request);
             }
         }).addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task1) {
-                if (requestsIDList.size() == 0) return;
+                if (requestsList.size() == 0) return;
 
                 int i = 0;
                 getSeparateUser(i);
             }
 
             private void getSeparateUser(final int i) {
-                userRef.child(requestsIDList.get(i)).get().addOnCompleteListener(task2 -> {
+                userRef.child(requestsList.get(i).getFromID()).get().addOnCompleteListener(task2 -> {
                     User user = task2.getResult().getValue(User.class);
 
                     if (user != null) {
                         Log.d("FRIENDS_INTER_FRAGMENT", user.getUs_name());
-                        requestsList.add(user);
-                        Log.d("FRIENDS_INTER_FRAGMENT added", requestsList.size() + "");
+                        requestersList.add(user);
+                        Log.d("FRIENDS_INTER_FRAGMENT added", requestersList.size() + "");
                     }
 
-                    if (i == requestsIDList.size() - 1) {
+                    if (i == requestsList.size() - 1) {
                         createAdapter();
                     } else {
                         getSeparateUser(i + 1);
@@ -110,7 +109,7 @@ public class FriendsInteractionsFragment extends Fragment {
     }
 
     private void createAdapter() {
-        requestsAdapter = new FriendRequestsAdapter(requestsList, requestsIDList, usId, getContext());
+        requestsAdapter = new FriendRequestsAdapter(requestersList, requestsList, usId, getContext());
         recyclerView.setAdapter(requestsAdapter);
     }
 }
